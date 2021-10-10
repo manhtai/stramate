9f024e85ba8d83899d5fe6699892ab421339b2ca
manhtai <vo@manhtai.com>
2021-10-10 11:42:13 +0700

    Import tasks

--- apps/activity/models.py
+++ apps/activity/models.py
@@ -1,23 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+FIELD_NAMES = [
+    "id", "name", "type", "description",
+    "distance", "moving_time", "total_elevation_gain", "average_speed",
+]
 
 
 class Activity(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
 
-    # Overview data
-    name = models.TextField()
-    type = models.TextField()
-    description = models.TextField()
+    name = models.TextField(blank=True)
+    type = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+
     distance = models.FloatField()  # m
     moving_time = models.PositiveIntegerField()  # s
     total_elevation_gain = models.FloatField()  # s
     average_speed = models.FloatField()  # m/s
-    start_date = models.DateTimeField()
 
-    start_lat = models.FloatField()
-    start_lon = models.FloatField()
-    start_location = models.JSONField()
+    start_date = models.DateTimeField()  # Timezone aware
+    start_location = models.TextField()  # place, region
 
     # Full data
     detail = models.JSONField()
     streams = models.JSONField()
+
+    # Relation
+    athlete_id = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @classmethod
+    def upsert(cls, user_id, detail, streams):
+        detail_dict = detail.to_dict()
+        streams_dict = {k: v.to_dict() for k, v in streams.items()}
+
+        defaults = {
+            **{k: detail_dict[k] or '' for k in FIELD_NAMES},
+            "start_date": detail.start_date,
+            "athlete_id": detail.athlete.id,
+            "user_id": user_id,
+            "detail": detail_dict,
+            "streams": streams_dict,
+        }
+
+        act, _ = Activity.objects.update_or_create(
+            **defaults,
+            defaults=defaults,
+        )
+        return act
+
+    def __str__(self):
+        return f"Activity {self.id}: {self.name}"
---
+++ apps/activity/tasks.py
@@ -0,0 +1,67 @@
+from huey import crontab
+from huey.contrib.djhuey import periodic_task, task
+from social_django.models import UserSocialAuth
+from stravalib.client import Client
+from django.conf import settings
+from datetime import timedelta
+from apps.activity.models import Activity
+import polyline
+import geocoder
+import traceback
+
+
+PROCESS_TIME = timedelta(minutes=5)
+PULL_LIMIT = 200
+STREAM_TYPES = ['time', 'latlng', 'distance', 'altitude', 'velocity_smooth',
+                'heartrate', 'cadence', 'watts', 'temp', 'moving', 'grade_smooth']
+
+
+def update_start_location(activity, line):
+    coords = polyline.decode(line)
+    g = geocoder.mapbox(coords[0], method='reverse')
+    start_location = f"{g.json['raw']['place']}, {g.json['raw']['region']}"
+    activity.start_location = start_location
+    activity.save()
+
+
+def import_activities(athlete_id):
+    client = Client()
+    auth_user = UserSocialAuth.objects.get_social_auth("strava", athlete_id)
+
+    client.access_token = auth_user.extra_data['access_token']
+    if auth_user.expiration_timedelta() < PROCESS_TIME:
+        res = client.refresh_access_token(
+            client_id=settings.SOCIAL_AUTH_STRAVA_KEY,
+            client_secret=settings.SOCIAL_AUTH_STRAVA_SECRET,
+            refresh_token=auth_user.extra_data['refresh_token'],
+        )
+        auth_user.extra_data['access_token'] = res['access_token']
+        auth_user.extra_data['expires'] = res['expires_at']
+        auth_user.save()
+
+    last_activity = Activity.objects.order_by('start_date').last()
+    last_import_time = last_activity and last_activity.start_date
+
+    for summary in client.get_activities(after=last_import_time, limit=PULL_LIMIT):
+        try:
+            detail = client.get_activity(summary.id, include_all_efforts=True)
+            streams = client.get_activity_streams(summary.id, types=STREAM_TYPES)
+            activity = Activity.upsert(auth_user.user_id, detail, streams)
+            print("Update Strava data for:", activity)
+
+            update_start_location(activity, detail.map.polyline)
+            print("Update start location for:", activity)
+        except Exception:
+            traceback.print_exc()
+
+
+@task
+def initial_import(athlete_id):
+    import_activities(athlete_id)
+
+
+@periodic_task(crontab(minute='*/5'))
+def check_for_new_activities():
+    # TODO: Use webhook for this
+    for ua in UserSocialAuth.objects.filter(provider='strava'):
+        import_activities(ua.uid)
