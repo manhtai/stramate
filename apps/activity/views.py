740519c831cf429365f4fc02d6e2d0ed95efb246
manhtai <vo@manhtai.com>
2021-10-11 18:39:16 +0700

    Map view

--- apps/activity/views.py
+++ apps/activity/views.py
@@ -1,8 +1,10 @@
 from django.http import FileResponse
+from django.views.decorators.cache import cache_control
 from django.core.files.storage import default_storage
 from apps.activity.models import Activity
 
 
+@cache_control(public=True, max_age=315_360_000)
 def map_view(request, activity_id):
     activity = Activity.objects.get(id=activity_id)
     file_path = activity.get_map_file()
