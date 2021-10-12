cf30943ea579aba5a224b3d9754e02e1c97ff357
manhtai <vo@manhtai.com>
2021-10-12 20:29:28 +0700

    Deployment (#5)
    * Change python ver
    
    * Move config
    
    * Remove unused lib
    
    * Wrong path
    
    * Running...
    
    * Add env

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Run once
+
+# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs supervisor
+# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+# sudo cp supervisor.conf /etc/supervisor/conf.d/
+# sudo service supervisor restart
+# poetry install
+# poetry run python manage.py tailwind init
+
+
+git pull
+
+poetry run python manage.py tailwind build
+poetry run python manage.py collectstatic --no-input
+poetry run python manage.py migrate
+sudo service supervisor restart stramate
