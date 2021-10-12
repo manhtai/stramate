f07d5813a40a1a50ff191c5e14fc94cc9b1d1ab6
manhtai <vo@manhtai.com>
2021-10-12 20:28:04 +0700

    Running...

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@
 # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs supervisor
 # curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+# sudo cp supervisor.conf /etc/supervisor/conf.d/
+# sudo service supervisor restart
 # poetry install
 # poetry run python manage.py tailwind init
-# cp supervisor.conf /etc/supervisor/conf.d/
 
 
 git pull
@@ -14,4 +15,4 @@ git pull
 poetry run python manage.py tailwind build
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
-sudo supervisor stramate restart
+sudo service supervisor restart stramate
