bafdc2fd6709f6740c7b977c1bd59136289fa36d
manhtai <vo@manhtai.com>
2021-10-12 20:07:52 +0700

    Move config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # Run once
+
+# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs supervisor
 # curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 # poetry install
-# poetry run honcho -f Procfile.prod export -a stramate supervisord /etc/supervisor/conf.d
+# poetry run python manage.py tailwind init
+# cp supervisor.conf /etc/supervisor/conf.d/
+
+
 git pull
+
+poetry run python manage.py tailwind build
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
-sudo supervisor restart stramate
+sudo supervisor stramate restart
