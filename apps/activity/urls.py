6ff2427e144997aceceff2c1ef5d7929e642ed4f
manhtai <vo@manhtai.com>
2021-10-14 19:52:14 +0700

    Init

--- apps/activity/urls.py
+++ apps/activity/urls.py
@@ -5,5 +5,6 @@ from apps.activity import views
 app_name = 'activity'
 
 urlpatterns = [
+    path('<int:activity_id>/3d', views.ThreeDView.as_view(), name='3d'),
     path('<int:activity_id>/map.png', views.map_view, name='map'),
 ]
--- apps/activity/views.py
+++ apps/activity/views.py
@@ -1,15 +1,31 @@
-from django.http import FileResponse, HttpResponse
-from django.views.decorators.cache import cache_control
 from django.core.files.storage import default_storage
+from django.http import FileResponse, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.cache import cache_control
+from django.views.generic import TemplateView
+
 from apps.activity.models import Activity
 
 
 @cache_control(public=True, max_age=315_360_000)
 def map_view(request, activity_id):
-    activity = Activity.objects.get(id=activity_id)
+    activity = get_object_or_404(Activity, id=activity_id)
     file_path = activity.get_map_file()
 
     if file_path:
         return FileResponse(default_storage.open(file_path, 'rb'))
 
     return HttpResponse('')
+
+
+class ThreeDView(TemplateView):
+    template_name = "page/map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        activity_id = kwargs.get('activity_id')
+        activity = get_object_or_404(Activity, id=activity_id)
+        context["activity"] = activity
+
+        return context
