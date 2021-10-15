73a6c317bf847b42808f069bafae8d1d8cc56895
manhtai <vo@manhtai.com>
2021-10-15 17:29:49 +0700

    Allow to edit...

--- apps/activity/views.py
+++ apps/activity/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from apps.activity.models import Activity
 
@@ -14,4 +15,16 @@ class RouteView(TemplateView):
         activity = get_object_or_404(Activity, id=activity_id)
         context["activity"] = activity
 
+        return context
+
+
+class DetailView(LoginRequiredMixin, TemplateView):
+    template_name = "activity/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        activity_id = kwargs.get('activity_id')
+        activity = get_object_or_404(Activity, id=activity_id, user_id=self.request.user.id)
+        context["activity"] = activity
         return context
