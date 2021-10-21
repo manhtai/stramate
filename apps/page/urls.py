0006a73b12bfab1ecc731c3ce8ef99298a9a9152
manhtai <vo@manhtai.com>
2021-10-21 12:50:06 +0700

    RSS

--- apps/page/urls.py
+++ apps/page/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.page.views import IndexView, ProfileView
+from apps.page.views import IndexView, ProfileView, RssView
 
 app_name = 'page'
 
 urlpatterns = [
+    path('<username>/rss.xml', RssView(), name='rss'),
     path('<username>', ProfileView.as_view(), name='profile'),
     path('', IndexView.as_view(), name='index'),
 ]
