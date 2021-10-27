c0a834668872f6cc1e9b5b02fb3ee5064d7f99ab
manhtai <vo@manhtai.com>
2021-10-27 19:16:06 +0700

    Allow admin sorts

--- apps/activity/admin.py
+++ apps/activity/admin.py
@@ -4,8 +4,11 @@ from apps.activity.models import Activity, Analytic
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'start_date', 'user']
-    search_fields = ['name']
+    list_display = [
+        '__str__', 'start_date', 'user',
+        'moving_time', 'distance', 'total_elevation_gain',
+    ]
+    search_fields = ['name', 'description', 'start_location']
     list_filter = ['start_date', 'type']
 
 
